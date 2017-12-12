ad0850ad8969c1b05e1213de05f95c9c79293c24
Praveen10566 <33379808+Praveen10566@users.noreply.github.com>
2017-12-11 21:51:17 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+// var app=angular.module("myApp",[]);
+// app.controller("myCtrl",function ($scope) {
+//     $scope.firstName='Bob';
+//     $scope.lastName='Rob';
+//
+// });
+
+//for name filter
+// var app=angular.module("myApp",[]);
+// app.controller("myCtrl",function ($scope) {
+//
+//     $scope.names=[
+//         "praveen",
+//         "kumar",
+//         "reddy",
+//         "Pavan",
+//         "yarramalli",
+//         "venkat"
+//     ]
+//
+// });
+
+
+//for Currency filter
+
+
+var app=angular.module("myApp",[]);
+app.controller('myCtrl',function ($scope) {
+
+    $scope.rate=[8,6,5,11,23,15,29,0];
+
+});
